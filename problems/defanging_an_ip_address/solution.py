e86de83a5c65b13623fc55b1f7891f119e7c6aae
Burak Aslantas <58150940+burakaslantas@users.noreply.github.com>
2021-02-22 19:34:27 +0000

    Sync LeetCode submission - Defanging an IP Address (python3)

--- problems/defanging_an_ip_address/solution.py
+++ problems/defanging_an_ip_address/solution.py
@@ -1,15 +1,3 @@
 class Solution:
     def defangIPaddr(self, address: str) -> str:
-        """
-        Solution #1
-        res = ""
-        for char in address:
-            if(char == "."):
-                res += "[.]"
-            else:
-                res += char
-        return res
-        """
-        
-        #Solution #2
-        return '[.]'.join(address.split("."))
+        return "[.]".join(address.split("."))
