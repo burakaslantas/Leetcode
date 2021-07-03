60956848e5e336695fe3944b4a20033866e11d6f
Burak Aslantas <58150940+burakaslantas@users.noreply.github.com>
2021-07-03 20:07:37 +0000

    Sync LeetCode submission - Two Sum (python3)

--- problems/two_sum/solution.py
+++ problems/two_sum/solution.py
@@ -1,8 +1,16 @@
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
-        buffer = {}
+        #choose first element
+        results = []
         for i in range(len(nums)):
-            if nums[i] in buffer:
-                return [buffer[nums[i]], i]
-            else:
-                buffer[target - nums[i]] = i
+            firstelement = nums[i]
+            for j in range(len(nums)):
+                if (i != j):
+                    secondelement = nums[j]
+                    if (firstelement + secondelement == target):
+                        results.append(i)
+                        results.append(j)
+                        return results
+        #use for loop start index 0 to index -1
+        #use another loop start index 0 to index -1
+        #but if i == j pass that index in another loop
