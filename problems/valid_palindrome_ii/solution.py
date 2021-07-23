4c47653dbdc3352bae1aecf301a832fe56568327
Burak Aslantas <58150940+burakaslantas@users.noreply.github.com>
2021-07-23 19:30:32 +0000

    Sync LeetCode submission - Valid Palindrome II (python3)

--- problems/valid_palindrome_ii/solution.py
+++ problems/valid_palindrome_ii/solution.py
@@ -1,5 +1,27 @@
 class Solution:
     def validPalindrome(self, s: str) -> bool:
+        """
+        Solution #1
+        def helper(s, l, r):
+            while(l < r):
+                if(s[l] != s[r]):
+                    return False
+                l += 1
+                r -= 1
+            return True
+            
+        l = 0
+        r = len(s) - 1
+        while(l < r):
+            if(s[l] != s[r]):
+                return helper(s, l+1, r) or helper(s, l, r-1)
+            l += 1
+            r -= 1
+        return True
+        """
+    
+        """
+        Solution #2
         l = 0
         r = len(s) - 1
         
@@ -11,3 +33,18 @@ class Solution:
             l += 1
             r -= 1
         return True
+        """
+        
+        #Solution #3
+        #Using Recursion
+        def valid(string, start, end, d): # d: num of chars you can delete at most
+            if(start >= end):
+                return True
+            if(string[start] == string[end]):
+                return valid(string, start+1, end-1, d)
+            else:
+                return d > 0 and (valid(string, start+1, end, d-1) or valid(string, start, end-1, d-1))
+            
+        return valid(s, 0, len(s)-1, 1)
+        
+        
