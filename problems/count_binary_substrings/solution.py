a8b6b0651fc3ae8f7e22c50111da47b79415676d
Burak Aslantas <58150940+burakaslantas@users.noreply.github.com>
2021-07-27 20:26:20 +0000

    Sync LeetCode submission - Count Binary Substrings (python3)

--- problems/count_binary_substrings/solution.py
+++ problems/count_binary_substrings/solution.py
@@ -1,16 +1,34 @@
 class Solution:
     def countBinarySubstrings(self, s: str) -> int:
+        """
+        Solution #1
         res = 0
+        sameTypes = []
         consecutiveCounter = 1
-        temp = []
         for i in range(1, len(s)):
-            if(s[i] == s[i-1]):
-                consecutiveCounter += 1
-            else:
-                temp += [consecutiveCounter]
+            if( s[i] != s[i-1] ):
+                sameTypes.append(consecutiveCounter)
                 consecutiveCounter = 1
-        temp += [consecutiveCounter]
+            else:
+                consecutiveCounter += 1
+        sameTypes.append(consecutiveCounter)
         
-        for i in range(1, len(temp)):
-            res += min(temp[i-1], temp[i])
+        for j in range(1, len(sameTypes)):
+            res += min( sameTypes[j-1], sameTypes[j] )
+        
         return res
+        """
+        
+        #Solution #2
+        answer, prev, curr = 0, 0, 1
+        #prev = preview consecutive counter
+        #curr = current consecutive counter
+        
+        for i in range(1, len(s)):
+            if( s[i-1] != s[i] ):
+                answer += min( prev, curr )
+                prev, curr = curr, 1
+            else:
+                curr += 1
+        return answer + min( prev, curr )
+        
