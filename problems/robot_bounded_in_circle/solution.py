7983c1262eaeb20738dff1030cb6e60e930dc3f6
Burak Aslantas <58150940+burakaslantas@users.noreply.github.com>
2021-08-14 14:15:05 +0000

    Sync LeetCode submission - Robot Bounded In Circle (python3)

--- problems/robot_bounded_in_circle/solution.py
+++ problems/robot_bounded_in_circle/solution.py
@@ -1,9 +1,10 @@
 class Solution:
     def isRobotBounded(self, instructions: str) -> bool:    
+        """
+        Solution #1
         directions = [ [0,1], [-1,0], [0,-1], [1,0] ] #[+y, -x, -y, +x]
         direction = 0
         location = [0,0]
-        circle = 0
         
         for instruction in instructions:
             print(direction)
@@ -16,5 +17,22 @@ class Solution:
                 direction -= 1
                 
         if(location == [0,0] or direction % 4 != 0):
+            return True
+        return False
+        """
+        
+
+        #Solution #2
+        moveX, moveY = 0, 1
+        x, y = 0, 0
+        for instruction in instructions:
+            if(instruction == 'R'):
+                moveX, moveY = moveY, -1 * moveX
+            elif(instruction == 'L'):
+                moveX, moveY = -1 * moveY, moveX
+            else:
+                x, y = x + moveX, y + moveY
+                
+        if( (x, y) == (0, 0) or (moveX, moveY) != (0, 1) ):
             return True
         return False
