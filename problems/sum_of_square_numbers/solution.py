6e7debd8d5e12f74bc53c41e8265dfa48c822168
Burak Aslantas <58150940+burakaslantas@users.noreply.github.com>
2021-08-25 19:24:51 +0000

    Sync LeetCode submission - Sum of Square Numbers (python3)

--- problems/sum_of_square_numbers/solution.py
+++ problems/sum_of_square_numbers/solution.py
@@ -11,11 +11,23 @@ class Solution:
         return False
         """
         
+        """
         #Solution #2
         upTo = int(sqrt(c)) + 1
         
         for b in range(upTo):
             a = sqrt( c - b ** 2 )
             if(a == int(a)):
+                return True
+        return False
+        """
+        
+        #Solution #3
+        upTo = int(sqrt(c)) + 1
+        squareNums = set([x * x for x in range(upTo)])
+        
+        for a2 in squareNums:
+            b2 = c - a2
+            if(b2 in squareNums):
                 return True
         return False
